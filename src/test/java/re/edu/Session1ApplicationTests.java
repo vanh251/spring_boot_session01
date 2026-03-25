5229573191af14a8044f2d3faf9d0580bcbe0e66
vanh251 <148326521+vanh251@users.noreply.github.com>
2026-03-26 02:30:04 +0700

    first commit

---
+++ src/test/java/re/edu/Session1ApplicationTests.java
@@ -0,0 +1,13 @@
+package re.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
