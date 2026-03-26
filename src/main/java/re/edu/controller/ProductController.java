61c2b24f8bf78d73322e83282cb5d255c6c79127
vanh251 <148326521+vanh251@users.noreply.github.com>
2026-03-26 12:25:00 +0700

    done

--- src/main/java/re/edu/controller/ProductController.java
+++ src/main/java/re/edu/controller/ProductController.java
@@ -1,4 +1,24 @@
 package re.edu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import re.edu.model.Product;
+import re.edu.service.ProductService;
+
+import java.util.List;
+
+@RestController
 public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/api/products")
+    public List<Product> showAllProducts(){
+        return productService.getAllProduct();
+    }
 }
--- src/main/java/re/edu/service/ProductService.java
+++ src/main/java/re/edu/service/ProductService.java
@@ -1,4 +1,22 @@
 package re.edu.service;
 
+import org.springframework.stereotype.Service;
+import re.edu.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ProductService {
+    private List<Product> productList = new ArrayList<>();
+
+    public ProductService() {
+        productList.add(new Product(1, "Laptop Dell XPS", 25000000));
+        productList.add(new Product(2, "Chuột Logitech G502", 1200000));
+        productList.add(new Product(3, "Bàn phím cơ Keychron", 2000000));
+    }
+
+    public List<Product> getAllProduct(){
+        return productList;
+    }
 }
